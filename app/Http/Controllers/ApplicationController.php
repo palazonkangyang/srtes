0f13d6c0122f1deb5ed304116d9a9770ba851744
oakkamyo <oakkamyo13@gmail.com>
2018-01-04 17:04:24 +0800

    v0.1.18

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -1653,7 +1653,7 @@ class ApplicationController extends ControllerCore
       $feedback['answers'] = $input['answer'];
       $feedback['description_title'] = $input['description_title'];
 
-      // dd($feedback['answer_input_type'][4]);
+      dd($feedback);
 
       for($i = 0; $i < count($feedback['answers']); $i ++)
       {
